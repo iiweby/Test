504ad7c0b68f9a0f2328f6e770aab6902dc96af1
Daniel Paredes <danielparedesmilian@gmail.com>
2022-08-08 06:33:15 -0800

    New R

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,24 +30,17 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
-	// if (
-	// 	calculationType !== "ADD" &&
-	// 	calculationType !== "SUBTRACT" &&
-	// 	calculationType !== "MULTIPLY" &&
-	// 	calculationType !== "DIVIDE"
-	// ) {
-	// 	return;
-	// }
-
+	const enteredNumber = getUserNumberInput();
 	if (
-		calculationType === "ADD" ||
-		calculationType === "SUBTRACT" ||
-		calculationType === "MULTIPLY" ||
-		calculationType === "DIVIDE"
+		(calculationType !== "ADD" &&
+			calculationType !== "SUBTRACT" &&
+			calculationType !== "MULTIPLY" &&
+			calculationType !== "DIVIDE") ||
+		!enteredNumber
 	) {
+		return;
 	}
 
-	const enteredNumber = getUserNumberInput();
 	const initialResult = currentResult;
 	let mathOperator;
 
